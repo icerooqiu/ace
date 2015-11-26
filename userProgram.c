207f3ccd71261b7041832410a066badeae488e46
Tianzi YANG <tyang15@trusty>
2015-11-26 16:18:42 -0500

    copyright

--- userProgram.c
+++ userProgram.c
@@ -3,12 +3,11 @@ struct test{
     int life;
 };
 int main(){
-    //struct test * what;
-    //what->life = 5;
+    struct test * what;
+    what->life = 5;
     int i = 0;
     for(i =0; i < 10; i++){
         printf("Why you no walk esther\n");
     }
-    system("mkdir TOMMYSUCKS");
     return 0;
 }
